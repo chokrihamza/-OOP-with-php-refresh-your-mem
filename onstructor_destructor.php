59e8da911b5329e3dab8ca91f23c7a78f1bf7076
chokrihamza <chokrihamza1994@gmail.com>
2021-05-31 12:07:31 +0200

    the basic of  oop php construc desctructl

--- onstructor_destructor.php
+++ onstructor_destructor.php
@@ -1,1 +1,32 @@
 <?php
+
+
+
+class Cars
+{
+  public $wheels_count = 4;
+  static $door_count = 4;
+
+  function __construct()
+  {
+    //echo $this->wheels_count;
+    echo self::$door_count++;
+  }
+
+  function __destruct()
+  {
+    //echo $this->wheels_count;
+    echo "boooom";
+  }
+}
+
+$bmw = new Cars();
+$bmw = null;
+$mercedes = new Cars();
+$mercedes = null;
+
+
+
+
+
+?>
