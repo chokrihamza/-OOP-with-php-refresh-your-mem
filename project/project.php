3e2c5775d6991efe97925e147449c39648760d96
chokrihamza <chokrihamza1994@gmail.com>
2021-06-04 13:36:37 +0200

    using composer

---
+++ project/project.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+$mail->SMTPDebug  = 1;
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "";
+$mail->Password   = "";
---
+++ project/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a04dd745cbe6a1f1b8b7b59d675be7f2993e8777',
+        'name' => 'hamza/chokri',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hamza/chokri' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a04dd745cbe6a1f1b8b7b59d675be7f2993e8777',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.4.1',
+            'version' => '6.4.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => '9256f12d8fb0cd0500f93b19e18c356906cbed3d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
